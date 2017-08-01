96783375659ac6fcc3b43831fc746134661718a0
benmcmurry <benmcmurry@gmail.com>
2017-08-01 13:01:04 -0600

    Fixed creation date issue

--- editors/new_passage.php
+++ editors/new_passage.php
@@ -1,7 +1,7 @@
 <?php
 include_once('../../../connectFiles/connect_sr.php');
 session_start();
-$new_passage = "Insert into Passages (title, creator, length, lexile) values('New Passage by ".$_SESSION['given_name']."','".$_SESSION['google_id']."','1','1')";
+$new_passage = "Insert into Passages (title, creator, length, lexile, date_created) values('New Passage by ".$_SESSION['given_name']."','".$_SESSION['google_id']."','1','1', now())";
 if(!$result = $db->query($new_passage)){
   die('There was an error running the query [' . $db->error . ']');
 } else {
