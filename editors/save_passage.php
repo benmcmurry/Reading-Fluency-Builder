ef2113f47e98fcfad07a8867602e8c794b0e1b10
benmcmurry <benmcmurry@gmail.com>
2017-08-22 12:55:54 -0600

    fixed double escaping

--- editors/save_passage.php
+++ editors/save_passage.php
@@ -8,22 +8,22 @@ if($_SESSION['logged_in'] == 'yes' && $_SESSION['editor'] == "1"){
 
 }
 include_once('../../../connectFiles/connect_sr.php');
-$passage_id = mysqli_real_escape_string($db, $_POST['passage_id']);
-$passage_title = mysqli_real_escape_string($db, $_POST['passage_title']);
-$passage_text = stripslashes(mysqli_real_escape_string($db, $_POST['passage_text']));
-$author = mysqli_real_escape_string($db, $_POST['author']);
-$source = mysqli_real_escape_string($db, $_POST['source']);
-$length = mysqli_real_escape_string($db, $_POST['length']);
-$lexile = mysqli_real_escape_string($db, $_POST['lexile']);
-$flesch_reading_ease = mysqli_real_escape_string($db, (float)$_POST['flesch_reading_ease']);
-$flesch_kincaid_level = mysqli_real_escape_string($db, (float)$_POST['flesch_kincaid_level']);
-$library_id = mysqli_real_escape_string($db, $_POST['library_id']);
-$vocabulary =  stripcslashes(mysqli_real_escape_string($db, $_POST['vocabulary']));
-$modified_by = mysqli_real_escape_string($db, $_POST['modified_by']);
+$passage_id =  $_POST['passage_id'];
+$passage_title =  $_POST['passage_title'];
+$passage_text = $_POST['passage_text'];
+$author =  $_POST['author'];
+$source =  $_POST['source'];
+$length =  $_POST['length'];
+$lexile =  $_POST['lexile'];
+$flesch_reading_ease =  (float)$_POST['flesch_reading_ease'];
+$flesch_kincaid_level =  (float)$_POST['flesch_kincaid_level'];
+$library_id =  $_POST['library_id'];
+$vocabulary =  stripcslashes( $_POST['vocabulary']);
+$modified_by =  $_POST['modified_by'];
 
 
-$passage_text = mysqli_real_escape_string($db, $passage_text);
-$vocabulary = mysqli_real_escape_string($db, $vocabulary);
+// $passage_text =  $passage_text);
+// $vocabulary =  $vocabulary);
 
 
 if (is_float($flesch_reading_ease)){} else {$flesch_reading_ease = 0;}
--- editors/save_question.php
+++ editors/save_question.php
@@ -8,14 +8,14 @@ if($_SESSION['logged_in'] == 'yes' && $_SESSION['editor'] == "1"){
 
 }
 include_once('../../../connectFiles/connect_sr.php');
-$question_id = mysqli_real_escape_string($db, $_POST['question_id']);
-$question_text = mysqli_real_escape_string($db, $_POST['question_text']);
-$correct_answer = mysqli_real_escape_string($db, $_POST['correct_answer']);
-$distractor_1 = mysqli_real_escape_string($db, $_POST['distractor_1']);
-$distractor_2 = mysqli_real_escape_string($db, $_POST['distractor_2']);
-$distractor_3 = mysqli_real_escape_string($db, $_POST['distractor_3']);
+$question_id =  $_POST['question_id'];
+$question_text =  $_POST['question_text'];
+$correct_answer =  $_POST['correct_answer'];
+$distractor_1 =  $_POST['distractor_1'];
+$distractor_2 =  $_POST['distractor_2'];
+$distractor_3 =  $_POST['distractor_3'];
 
-$modified_by = mysqli_real_escape_string($db, $_POST['modified_by']);
+$modified_by =  $_POST['modified_by'];
 
 
 
