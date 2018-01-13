9dac7aaa46901fdf5b367ed3a04e5a8b4d1603b2
benmcmurry <benmcmurry@gmail.com>
2018-01-13 15:57:14 -0700

    netid fix

--- netid.php
+++ netid.php
@@ -2,7 +2,7 @@
 session_start();
 include_once('../../connectFiles/connect_sr.php');
 
-$search_for_netid = $sr_db->prepare("Select netid from Netids where netid=?");
+$search_for_netid = $sr_db->prepare("Select netid from netids where netid=?");
 $search_for_netid->bind_param("s", $_POST['netid']);
 $search_for_netid->execute();
 $results = $search_for_netid->get_result();
